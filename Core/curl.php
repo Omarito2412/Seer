c17567766a017d5fc7269fa4300dd46cb69c8710
Omar Essam <omar_2412@live.com>
2015-05-31 20:18:25 +0200

    Implemented basic CURL functionalities
    Signed-off-by: Omar Essam <omar_2412@live.com>

--- Core/curl.php
+++ Core/curl.php
@@ -4,6 +4,44 @@
 *         Connects to website and grabs documents
 *         Organize options and header types, Also request types and data
 */
-class curl{
+class curl
+{
+     private $handle;
+     private $options;
+     private $page;
+     private $errors;
+     private $errors_count;
 
+     public curl($url){
+          $this->handle = curl_init($url);
+     }
+
+     public add_option($key, $value = null){
+          if($value == null) {      // Options are supplied through an array
+               foreach($key as $option => $value){
+                    $this->options[$option] = $value;
+               }
+          }
+          else {
+               $this->options[$key] = $value;
+          }
+     }
+
+     public function run(){
+          foreach($this->options as $option => $value) {
+               curl_setopt($handle, $option, $value);
+          }
+          $this->page = curl_exec($this->handle);
+          if(($this->errors_count = curl_errno($this->handle)) > 0) {
+               $this->errors = curl_error($this->handle);
+          }
+          curl_close($this->handle);
+     }
+
+     public function retrieve_errors(){
+          return array(
+               'count' => $this->errors_count;
+               'errors' => $this->errors;
+          );
+     }
 }
