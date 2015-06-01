e604c5abca98e073993bf382f30089c60ebe7374
Omar Essam <omar_2412@live.com>
2015-06-01 16:33:30 +0200

    Implemented output CSV
    Signed-off-by: Omar Essam <omar_2412@live.com>

--- Core/output.php
+++ Core/output.php
@@ -5,16 +5,36 @@
 */
 class output{
      private $input;
+     private $info
 
-     public function __construct($input, $separator = null){
-          if($separator == null){       //Input is a list
-               $cursor = 0;
-               foreach($input as $node){
-                    $this->input[$cursor] =
-               }
-          }
-          else{     //Input is a separated string
+     public function csv_init($name, $headers, $separator=","){
+          if($headers != FALSE)
+               $input[$name] = array(
+                         'headers' => $headers
+               );
 
+          $info[$name] = array(
+                    'separator' => $separator
+          );
+     }
+     public function csv_push($entry){       //$entry = array('col1' => val1, 'col2' => val2); CSV Columns of 1 row
+          $input[$name][] = implode($info[$name]['separator'], $entry);
+     }
+     public function csv_push_all($lines_array){  //$lines_array = array(array('col1' => val1), array('col1'=> val1)); CSV Rows
+          foreach($lines_array as $entry){
+               $input[$name][] = implode($info[$name]['separator'], $entry);
           }
+     }
+     public function csv_create($name){
+          $fh = fopen($name, "w+");
+          $csv = implode("\n", $input[$name]);
+          fwrite($fh, $csv);
+          fclose($fh);
+     }
+     public function csv_append($name){
+          $fh = fopen($name, "a+");
+          $csv = implode("\n", $input[$name]);
+          fwrite($fh, $csv);
+          fclose($fh);
      }
 }
