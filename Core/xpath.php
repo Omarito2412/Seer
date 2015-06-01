0713dcd4a7ac72599c7159e048f53a2eef57b295
Omar Essam <omar_2412@live.com>
2015-06-01 02:07:01 +0200

    Create Xpath::run()
    Signed-off-by: Omar Essam <omar_2412@live.com>

--- Core/xpath.php
+++ Core/xpath.php
@@ -11,6 +11,7 @@ class xpath
      private $xpath;
      private $query_map;
      private $return_types;
+     private $output_buffer;
 
      public function xpath($html){
           libxml_use_internal_errors(true);
@@ -29,12 +30,25 @@ class xpath
                foreach($input as $name => $query){
                     $query_data = explode("#", $query);     //Concatenating the Query and the return type with a hash tag
                     $this->query_map[$name] = $query_data[0];
-                    $this->return_types[$name] = $query_data[1];
+                    if(isset($query_data[1]))
+                         $this->return_types[$name] = $query_data[1];
+                    else
+                         $this->return_types[$name] = "LIST";
                }
           }
           else{
                $this->query_map[$input] = $query;
                $this->return_types[$input] = $return;
           }
+     }
+     public function run(){
+          foreach($this->query_map as $name => $query){
+               $this->output_buffer[$name] = organize($this->xpath->query($query), $this->return_types[$name]);
+          }
+          return $this->output_buffer;
+     }
+     protected function organize($xpath_result, $type){
+          if($type == "LIST")
+               return $xpath_result;
      }
 }
