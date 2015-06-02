7424224f4da9e3f87c07d988b9634b27a2ca58d1
samayo <equbazghi@yahoo.com>
2015-06-02 07:45:49 +0200

    Add dockblock, Namespaces, Proper code indents

--- Core/curl.php
+++ Core/curl.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Seer, 
+ * Seer,
  *
  * A PHP XPath based web scraping framework.
  *
@@ -15,49 +15,52 @@
 namespace Seer;
 
 /**
- * Responsibilites:
+ * Responsibilities:
  *   Connects to website and grabs documents
  *   Organize options and header types, Also request types and data
  */
 class Curl
 {
-     private $handle;
-     private $options;
-     private $page;
-     private $errors;
-     private $errors_count;
+    private $handle;
+    private $options;
+    private $page;
+    private $errors;
+    private $errors_count;
 
-     public function init($url){
-          $this->handle = curl_init($url);
-     }
+    public function init($url)
+    {
+        $this->handle = curl_init($url);
+    }
 
-     public function add_option($key, $value = null){
-          if($value == null) {      // Options are supplied through an array
-               foreach($key as $option => $value){
-                    $this->options[$option] = $value;
-               }
-          }
-          else {
-               $this->options[$key] = $value;
-          }
-     }
+    public function add_option($key, $value = null)
+    {
+        if ($value == null) {      // Options are supplied through an array
+            foreach ($key as $option => $value) {
+                $this->options[$option] = $value;
+            }
+        } else {
+            $this->options[$key] = $value;
+        }
+    }
 
-     public function run(){
-          foreach($this->options as $option => $value) {
-               curl_setopt($this->handle, $option, $value);
-          }
-          $this->page = curl_exec($this->handle);
-          if(($this->errors_count = curl_errno($this->handle)) > 0) {
-               $this->errors = curl_error($this->handle);
-          }
-          curl_close($this->handle);
-          return $this->page;
-     }
+    public function run()
+    {
+        foreach ($this->options as $option => $value) {
+            curl_setopt($this->handle, $option, $value);
+        }
+        $this->page = curl_exec($this->handle);
+        if (($this->errors_count = curl_errno($this->handle)) > 0) {
+            $this->errors = curl_error($this->handle);
+        }
+        curl_close($this->handle);
+        return $this->page;
+    }
 
-     public function retrieve_errors(){
-          return array(
-               'count' => $this->errors_count,
-               'errors' => $this->errors
-          );
-     }
+    public function retrieve_errors()
+    {
+        return array(
+            'count' => $this->errors_count,
+            'errors' => $this->errors
+        );
+    }
 }
--- Core/xpath.php
+++ Core/xpath.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Seer, 
+ * Seer,
  *
  * A PHP XPath based web scraping framework.
  *
@@ -14,59 +14,67 @@
  */
 namespace Seer;
 
-use \DOMXpath, 
-    \DOMDocument; 
+use \DOMXpath,
+    \DOMDocument;
+
 /**
- * Responsibilites:
+ * Responsibilities:
  *    Build DOMXpath object
  *    Assign/Execute queries
  *    Clean and organize outputs based on filters
  */
-
 class Xpath
 {
-     private $errors;
-     private $xpath;
-     private $query_map;
-     private $return_types;
-     private $output_buffer;
+    private $errors;
+    private $xpath;
+    private $query_map;
+    private $return_types;
+    private $output_buffer;
 
-     public function load($html){
-          libxml_use_internal_errors(true);
-          $DOM = new DOMDocument;
-          if(!$DOM->loadHTML($html)) {
-               $this->errors = libxml_get_errors();
-               libxml_clear_errors();
-          }
-          $this->xpath = new DOMXPath($DOM);
-     }
-     public function get_errors(){
-          return $this->errors;
-     }
-     public function add_query($input, $query=null, $return="LIST"){
-          if($query == null){      //Queries were supplied in an array.
-               foreach($input as $name => $query){
-                    $query_data = explode("#", $query);     //Concatenating the Query and the return type with a hash tag
-                    $this->query_map[$name] = $query_data[0];
-                    if(isset($query_data[1]))
-                         $this->return_types[$name] = $query_data[1];
-                    else
-                         $this->return_types[$name] = "LIST";
-               }
-          }
-          else{
-               $this->query_map[$input] = $query;
-               $this->return_types[$input] = $return;
-          }
-     }
-     public function run(){
-          foreach($this->query_map as $name => $query){
-               $this->output_buffer[$name] = $this->organize($this->xpath->query($query), $this->return_types[$name]);
-          }
-          return $this->output_buffer;
-     }
-     protected function organize($xpath_result, $type){
-          if($type == "LIST")
-               return $xpath_result;
-     }
+    public function load($html)
+    {
+        libxml_use_internal_errors(true);
+        $DOM = new DOMDocument;
+        if (!$DOM->loadHTML($html)) {
+            $this->errors = libxml_get_errors();
+            libxml_clear_errors();
+        }
+        $this->xpath = new DOMXPath($DOM);
+    }
+
+    public function get_errors()
+    {
+        return $this->errors;
+    }
+
+    public function add_query($input, $query = null, $return = "LIST")
+    {
+        if ($query == null) {      //Queries were supplied in an array.
+            foreach ($input as $name => $query) {
+                $query_data = explode("#", $query);     //Concatenating the Query and the return type with a hash tag
+                $this->query_map[$name] = $query_data[0];
+                if (isset($query_data[1]))
+                    $this->return_types[$name] = $query_data[1];
+                else
+                    $this->return_types[$name] = "LIST";
+            }
+        } else {
+            $this->query_map[$input] = $query;
+            $this->return_types[$input] = $return;
+        }
+    }
+
+    public function run()
+    {
+        foreach ($this->query_map as $name => $query) {
+            $this->output_buffer[$name] = $this->organize($this->xpath->query($query), $this->return_types[$name]);
+        }
+        return $this->output_buffer;
+    }
+
+    protected function organize($xpath_result, $type)
+    {
+        if ($type == "LIST")
+            return $xpath_result;
+    }
 }
