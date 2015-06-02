11e4e50ee1fd602ea9f3846b812f37479bedb657
samayo <equbazghi@yahoo.com>
2015-06-02 07:46:39 +0200

    add namespace, and format code

--- Seer.php
+++ Seer.php
@@ -1,23 +1,39 @@
 <?php
-/*
-*    Responsibilites:
-*         This is the loader class, Communication occurs only through it.
-*/
+/**
+ * Seer,
+ *
+ * A PHP XPath based web scraping framework.
+ *
+ * PHP support 5.3+
+ *
+ * @package     Seer
+ * @version     0.0.0
+ * @author      Omarito2412
+ * @link        https://github.com/Omarito2412/Seer
+ * @license     MIT
+ */
+namespace Seer;
 
+/**
+ * Responsibilites:
+ *   This is the loader class, Communication occurs only through it.
+ */
 define('CORE_LOCATION', "./Core/");
 
-require_once CORE_LOCATION.'curl.php';
-require_once CORE_LOCATION.'xpath.php';
-require_once CORE_LOCATION.'output.php';
+require_once CORE_LOCATION . 'curl.php';
+require_once CORE_LOCATION . 'xpath.php';
+require_once CORE_LOCATION . 'output.php';
 
-class Seer{
-     public $xpath;
-     public $curl;
-     public $output;
+class Seer
+{
+    public $xpath;
+    public $curl;
+    public $output;
 
-     public function __construct(){
-          $this->xpath = new xpath();
-          $this->curl = new curl();
-          $this->output = new output();
-     }
+    public function __construct()
+    {
+        $this->xpath = new xpath();
+        $this->curl = new curl();
+        $this->output = new output();
+    }
 }
